6201441495546f7ffcf6c3d24eb5b9fd2e06c3c1
Gonzalo-Busson <49691754+Gonzalo-Busson@users.noreply.github.com>
2023-06-19 14:49:00 -0300

    mejora

--- src/com/company/neovest/qa/EncontrarOperacionesImplementacion.java
+++ src/com/company/neovest/qa/EncontrarOperacionesImplementacion.java
@@ -1,36 +1,43 @@
 package com.company.neovest.qa;
 
 import java.util.ArrayList;
+import java.util.Stack;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class EncontrarOperacionesImplementacion implements EncontrarOperacionesInterface {
     @Override
-    public ArrayList<String> obtenerOperaciones(ArrayList<Integer> var1, ArrayList<Operadores> var2, int var3, int var4) {
+    public ArrayList<String> obtenerOperaciones(ArrayList<Integer> var1, ArrayList<Operadores> var2, int var3, int var4) throws ScriptException {
        ArrayList<Integer> numeros_usados = new ArrayList<>();
-       ArrayList<Operadores> operadoes_usados = new ArrayList<>();
+       ArrayList<Integer> operadoes_usados = new ArrayList<>();
        ArrayList<StringBuilder> expresiones = new ArrayList<>();
 
-       Backtracking(var1, var3, var2, var4, numeros_usados, operadoes_usados, expresiones);
+       if (var3 > var2.size()){
+           Backtracking(var1, var3, var2, var4, numeros_usados, operadoes_usados, expresiones);
+       }
        System.out.println(expresiones);
        return null;
     }
 
-    private void Backtracking(ArrayList<Integer> numeros, int cantidad_numeros, ArrayList<Operadores> operadores, int objetivo, ArrayList<Integer> numeros_usados, ArrayList<Operadores> operadoes_usados, ArrayList<StringBuilder> expresiones) {;
+    private void Backtracking(ArrayList<Integer> numeros, int cantidad_numeros, ArrayList<Operadores> operadores, int objetivo, ArrayList<Integer> numeros_usados, ArrayList<Integer> operadoes_usados, ArrayList<StringBuilder> expresiones) throws ScriptException {;
         for (int i=0 ; i<numeros.size();i++){
             if (!numeros_usados.contains(i)){
                 numeros_usados.add(i);
                 cantidad_numeros=cantidad_numeros-1;
                 if (cantidad_numeros==0){
-                    System.out.println((obtenerExpresion(numeros,numeros_usados,operadoes_usados)+" = "+calcularExpresion(numeros,numeros_usados,operadoes_usados)));
-                    if (calcularExpresion(numeros,numeros_usados,operadoes_usados)==objetivo ){
-                        expresiones.add(obtenerExpresion(numeros,numeros_usados,operadoes_usados));
+                    if (calcularExpresion(numeros,numeros_usados,operadores,operadoes_usados)==objetivo ){
+                        expresiones.add(obtenerExpresion(numeros,numeros_usados,operadores,operadoes_usados));
                     }
                 }else {
                     if (cantidad_numeros>0){
                         for (int j=0 ; j<operadores.size();j++){
-                            operadoes_usados.add(operadores.get(j));
-                            Backtracking(numeros,cantidad_numeros,operadores,objetivo,numeros_usados,operadoes_usados,expresiones);
-                            operadoes_usados.remove(operadores.get(j)) ;
-
+                            if (!operadoes_usados.contains(j)){
+                                operadoes_usados.add(j);
+                                Backtracking(numeros,cantidad_numeros,operadores,objetivo,numeros_usados,operadoes_usados,expresiones);
+                                operadoes_usados.remove(operadoes_usados.indexOf(j)) ;
+                            }
                         }
                     }
                 }
@@ -40,41 +47,49 @@ public class EncontrarOperacionesImplementacion implements EncontrarOperacionesI
         }
     }
 
-    private StringBuilder obtenerExpresion(ArrayList<Integer> numeros, ArrayList<Integer> numeros_usados, ArrayList<Operadores> operadoes_usados) {
+    private StringBuilder obtenerExpresion(ArrayList<Integer> numeros, ArrayList<Integer> numeros_usados, ArrayList<Operadores> operadores, ArrayList<Integer> operadoes_usados) {
         StringBuilder expreison= new StringBuilder();
         for (int i=0 ; i < numeros_usados.size();i++) {
             expreison.append(numeros.get(numeros_usados.get(i)));
             if (i < operadoes_usados.size()){
-                expreison.append("").append(operadoes_usados.get(i)).append("");
+                if (operadores.get(operadoes_usados.get(i)) == Operadores.MULTI)expreison.append("").append("*").append("");
+                if (operadores.get(operadoes_usados.get(i)) == Operadores.DIV)expreison.append("").append("/").append("");
+                if (operadores.get(operadoes_usados.get(i)) == Operadores.SUMA)expreison.append("").append("+").append("");
+                if (operadores.get(operadoes_usados.get(i)) == Operadores.RESTA)expreison.append("").append("-").append("");
+
+
+
             }
         }
         return expreison;
     }
-    public int calcularExpresion(ArrayList<Integer> numeros, ArrayList<Integer> numeros_usados, ArrayList<Operadores> operadoes_usados){
-        int resultado = numeros.get(numeros_usados.get(0));
-        for (int i = 0; i< operadoes_usados.size();i++){
+    public int calcularExpresion(ArrayList<Integer> numeros, ArrayList<Integer> numeros_usados, ArrayList<Operadores> operadores,ArrayList<Integer> operadoes_usados) throws ScriptException {
 
-                Operadores operador = operadoes_usados.get(i);
+        StringBuilder ecuacion = obtenerExpresion(numeros,numeros_usados,operadores,operadoes_usados);
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        Object result = engine.eval(String.valueOf(ecuacion));
+        int value= 0;
+        if (result instanceof Double) {
+            double doubleValue = (Double) result;
+            value = (int) doubleValue;
+        }else {
+            value = (int) result;
+        }
+        System.out.println(ecuacion+" = "+ (value) );
+        return value;
+        }
 
-                int numero= numeros.get(numeros_usados.get(i+1));
 
-                if (operador.equals(Operadores.MULTI)||operador.equals(Operadores.DIV)){
-                    int proxNumero = numeros.get(numeros_usados.get(i)+2);
 
-                    if (operador.equals(Operadores.MULTI)){
-                        resultado = numero * proxNumero;
-                    }else {
-                        resultado = numero/proxNumero;
-                    }
-                    i++;
-                }else {
-                    if (operador.equals(Operadores.SUMA))
-                        resultado += numero;
-                    else resultado -= numero;
-                }
-            }
-
-        return resultado;
-    }
 
 }
+
+
+
+
+
+
+
+
+
--- src/com/company/neovest/qa/Main.java
+++ src/com/company/neovest/qa/Main.java
@@ -1,10 +1,24 @@
 package com.company.neovest.qa;
 
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+import javax.script.ScriptException;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ScriptException {
+       // test1();
+       // tes2();
+        //tes3();
+        test4();
+        test5();
+    }
+
+
+
+    public static void test1() throws ScriptException {
         ArrayList<Integer> numeros = new ArrayList<>();
         ArrayList<Operadores> operadores = new ArrayList<>();
         //caso 1
@@ -16,9 +30,10 @@ public class Main {
         operadores.add(Operadores.SUMA);
         EncontrarOperacionesInterface operacionesImplementacion= new EncontrarOperacionesImplementacion();
         operacionesImplementacion.obtenerOperaciones(numeros,operadores,3,1);
+    }
 
-        //caso 2
-        ArrayList<Integer> numeros1 = new ArrayList<>();
+    public static void tes2() throws ScriptException {
+        ArrayList<Integer> numeros1 = new ArrayList<>(){};
         ArrayList<Operadores> operadores1 = new ArrayList<>();
 
         numeros1.add(2);
@@ -27,8 +42,55 @@ public class Main {
         numeros1.add(5);
         operadores1.add(Operadores.SUMA);
         operadores1.add(Operadores.MULTI);
+        EncontrarOperacionesInterface operacionesImplementacion= new EncontrarOperacionesImplementacion();
         operacionesImplementacion.obtenerOperaciones(numeros1,operadores1,3,12);
-
-
     }
+
+    public static void tes3() throws ScriptException {
+        ArrayList<Integer> numeros1 = new ArrayList<>(){};
+        ArrayList<Operadores> operadores1 = new ArrayList<>();
+
+        numeros1.add(2);
+        numeros1.add(3);
+        numeros1.add(4);
+        numeros1.add(5);
+        operadores1.add(Operadores.SUMA);
+        operadores1.add(Operadores.MULTI);
+        operadores1.add(Operadores.RESTA);
+        operadores1.add(Operadores.DIV);
+        EncontrarOperacionesInterface operacionesImplementacion= new EncontrarOperacionesImplementacion();
+        operacionesImplementacion.obtenerOperaciones(numeros1,operadores1,2,50);
+    }
+
+    public static void test4() throws ScriptException {
+        ArrayList<Integer> numeros1 = new ArrayList<>(){};
+        ArrayList<Operadores> operadores1 = new ArrayList<>();
+
+        numeros1.add(2);
+        numeros1.add(3);
+        numeros1.add(4);
+        numeros1.add(5);
+        operadores1.add(Operadores.SUMA);
+        operadores1.add(Operadores.MULTI);
+        operadores1.add(Operadores.DIV);
+        EncontrarOperacionesInterface operacionesImplementacion= new EncontrarOperacionesImplementacion();
+        operacionesImplementacion.obtenerOperaciones(numeros1,operadores1,4,13);
+    }
+
+    private static void test5() throws ScriptException {
+        ArrayList<Integer> numeros1 = new ArrayList<>(){};
+        ArrayList<Operadores> operadores1 = new ArrayList<>();
+
+        numeros1.add(2);
+        numeros1.add(3);
+        numeros1.add(4);
+        numeros1.add(0);
+        operadores1.add(Operadores.SUMA);
+        operadores1.add(Operadores.MULTI);
+        operadores1.add(Operadores.DIV);
+        EncontrarOperacionesInterface operacionesImplementacion= new EncontrarOperacionesImplementacion();
+        operacionesImplementacion.obtenerOperaciones(numeros1,operadores1,4,2);
+    }
+
+
 }
